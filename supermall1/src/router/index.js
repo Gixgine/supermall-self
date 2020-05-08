494430476447dc526829d04ad7ec8eb7d646edd3
Gixgine <2356224568@qq.com>
2020-05-08 17:30:10 +0800

    路由更新

--- supermall1/src/router/index.js
+++ supermall1/src/router/index.js
@@ -1,44 +1,50 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-const Home = () => import("../views/home/Home")
-const Cart = () => import("../views/cart/Cart")
-const Category = () => import("../views/category/Category")
-const Profile = () => import("../views/profile/Profile")
-const Detail = () => import("../views/detail/Detail")
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
+const Detail = () => import('../views/detail/Detail')
 
+// 修改路由replace方法,阻止重复点击报错
+const originalReplace = VueRouter.prototype.replace
+VueRouter.prototype.replace = function replace (location) {
+  return originalReplace.call(this, location).catch(err => err)
+}
+// 1.安装插件
 Vue.use(VueRouter)
-
+// 2.创建router
 const routes = [
   {
-    path:"",
-    redirect:"/home"
+    path: '',
+    redirect: '/home'
   },
   {
-    path:"/home",
-    component:Home
+    path: '/home',
+    component: Home
   },
   {
-    path:"/cart",
-    component:Cart
+    path: '/category',
+    component: Category
   },
   {
-    path:"/category",
-    component:Category
+    path: '/cart',
+    component: Cart
   },
   {
-    path:"/profile",
-    component:Profile
+    path: '/profile',
+    component: Profile
   },
   {
-    path:"/detail/:iid",
-    component:Detail
+    path: '/detail/:iid',
+    component: Detail
   }
 ]
 
 const router = new VueRouter({
-  routes,
-  mode:"history"
+  // mode: 'history',
+  routes
 })
 
 export default router
